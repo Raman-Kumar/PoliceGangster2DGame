24edbb3400beb267835c7e3c3fb23886263a44cf
Raman Kumar <ramankumar@Ramans.local>
2019-05-28 14:34:52 +0530

    First commit
    Police Gangster 2D

---
+++ PoliceGangster/Assets/Gangster.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gangster : MonoBehaviour
+{
+    private float deltaX, deltaY;
+    float dirX, dirY;
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetAxis("Horizontal") != 0 ||
+            Input.GetAxis("Vertical") != 0){
+
+        dirX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        dirY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.position = new Vector2 (transform.position.x + dirX, transform.position.y + dirY );
+            }
+
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)){
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+                break;
+
+                case TouchPhase.Moved:
+                    if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)){
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    }
+                break;
+                
+                // case TouchPhase.Ended:
+                // break;
+            }
+        }
+    }
+}
---
+++ PoliceGangster/Assets/Police.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject traget;
+    Vector3 directionToTarget;
+    public float moveSpeed = 2f;
+    bool follow = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        traget = GameObject.Find("gangsterE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(follow){
+            directionToTarget = (traget.transform.position - transform.position).normalized;
+            rb.velocity = new Vector2(directionToTarget.x * moveSpeed, directionToTarget.y * moveSpeed);
+        }else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        follow = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        follow = false;
+    }
+}
